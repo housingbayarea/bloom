3c532cec8897031bf4919fe2e584896bd34d3699
seanmalbert <smabert@gmail.com>
2021-09-30 17:30:49 -0700

    adds sentry auth token

--- sites/public/sentry.server.config.js
+++ sites/public/sentry.server.config.js
@@ -2,6 +2,12 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("dotenv").config()
+}
+
 import * as Sentry from "@sentry/nextjs"
 
 // eslint-disable-next-line no-undef
@@ -11,6 +17,8 @@ Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.2,
+  // eslint-disable-next-line no-undef
+  token: process.env.SENTRY_AUTH_TOKEN,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
