ce78401b2adc07362bcc95a8aab33d89bb01b841
Jared White <jared@jaredwhite.com>
2022-05-15 23:49:12 -0700

    chore: add translations for How it Works content

--- sites/public/pages/index.tsx
+++ sites/public/pages/index.tsx
@@ -18,6 +18,7 @@ import { ConfirmationModal } from "../src/ConfirmationModal"
 import { MetaTags } from "../src/MetaTags"
 import { fetchJurisdictionByName } from "../lib/hooks"
 import Markdown from "markdown-to-jsx"
+import RenderIf from "../components/RenderIf"
 
 import pageContent from "../page_content/homepage_how_it_works.md"
 
@@ -110,6 +111,7 @@ export default function Home(props: IndexProps) {
           <Markdown
             options={{
               overrides: {
+                RenderIf,
                 ol: {
                   component: ({ ...props }) => (
                     <ol {...props} className="process-list has-horizontal-layout" />
