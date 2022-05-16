7df0e40f3db96ab95a5f1df9b508df9442cb2410
Yazeed Loonat <yazeedloonat@gmail.com>
2022-05-16 16:13:48 -0700

    fix: removing duplicate migration

--- backend/core/src/migration/1650934163344-alameda-programs-and-preferences.ts
+++
@@ -1,8 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-
-export class alamedaProgramsAndPreferences1650934163344 implements MigrationInterface {
-  name = "alamedaProgramsAndPreferences1650934163344"
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
