503b3a216b52c454b3d03f99b4ac29fabe98b0e4
ColinBuyck <colin.buyck@gmail.com>
2022-08-29 16:41:27 -0700

    fix: import support for new csv structure

--- sites/public/scripts/resource-translations-import.ts
+++ sites/public/scripts/resource-translations-import.ts
@@ -10,42 +10,54 @@ async function main() {
     return
   }
   const json = await csv({ delimiter: "," }).fromFile(process.argv[2])
+  console.log(json)
+  let fileName = ""
+  let resource = ""
+  let writePath = ""
   for (const row of json) {
-    const writePath = path.resolve("", `sites/public/page_content/resources/${row.file}`)
-    let resource = ""
+    writePath = path.resolve("", `sites/public/page_content/resources/${row.file}`)
+    if (fileName !== row.file && writePath !== "") {
+      fs.writeFileSync(writePath, resource)
+      resource = ""
+      fileName = row.file
+    }
 
     const style = JSON.parse(`{"md":"${row.style}"}`)
 
     if (style.md.length) {
-      resource = style.md
+      resource += style.md
     }
 
-    const emptyTrans = ["en"]
+    // const emptyTrans = ["en"]
 
-    for (const key in row) {
-      if (key !== "file" && key !== "style" && row[key].length === 0) {
-        emptyTrans.push(key)
-      }
-    }
+    // for (const key in row) {
+    //   if (key !== "file" && key !== "style" && row[key].length === 0) {
+    //     emptyTrans.push(key)
+    //   }
+    // }
 
-    const en = JSON.parse(`{"md":"${row["String to Translate"]}"}`)
+    let section = `<RenderIf language="${row.lang}">\n`
+    row.url ? (section += `### [${row.title}] (${row.url}) \n`) : (section += `### ${row.title} \n`)
+    section += `\n ${row.body} \n`
+    section += "</RenderIf>\n"
+    resource += section
+    // for (const key in row) {
+    //   if (
+    //     key === "file" ||
+    //     key === "style" ||
+    //     key === "String to Translate" ||
+    //     row[key].length === 0
+    //   )
+    //     continue
+    //   const parsed = JSON.parse(`{"md":"${row[key]}"}`)
+    //   resource += `<RenderIf language="${key}">${parsed.md}</RenderIf>\n`
+    // }
 
-    resource += `<RenderIf language="${emptyTrans.join(",")}">${en.md}</RenderIf>\n`
-
-    for (const key in row) {
-      if (
-        key === "file" ||
-        key === "style" ||
-        key === "String to Translate" ||
-        row[key].length === 0
-      )
-        continue
-      const parsed = JSON.parse(`{"md":"${row[key]}"}`)
-      resource += `<RenderIf language="${key}">${parsed.md}</RenderIf>\n`
-    }
-
-    fs.writeFileSync(writePath, resource)
+    // fs.writeFileSync(writePath, resource)
   }
+  console.log("hmmmm")
+  console.log(writePath, resource)
+  fs.writeFileSync(writePath, resource)
 }
 
 void main()
