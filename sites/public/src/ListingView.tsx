e6eb6c4d1f133c8dbd254d8d9f17688d0c94f088
Emily Jablonski <emily.jablonski@exygy.com>
2022-09-08 18:55:51 -0400

    feat: toggle what to expect field based on listing type unless custom string exists

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -12,6 +12,7 @@ import {
   ListingAvailability,
   Jurisdiction,
   ApplicationSection,
+  ListingReviewOrder,
 } from "@bloom-housing/backend-core/types"
 import {
   AdditionalFees,
@@ -342,6 +343,26 @@ export const ListingView = (props: ListingProps) => {
   const getDateString = (date: Date, format: string) => {
     return date ? dayjs(date).format(format) : null
   }
+
+  const getWhatToExpectContent = () => {
+    if (listing.whatToExpect) return { content: listing.whatToExpect, expandableContent: null }
+    if (listing.reviewOrderType === ListingReviewOrder.lottery)
+      return {
+        content: t("whatToExpect.lottery"),
+        expandableContent: t("whatToExpect.lotteryReadMore"),
+      }
+    if (listing.listingAvailability === ListingAvailability.openWaitlist)
+      return {
+        content: t("whatToExpect.waitlist"),
+        expandableContent: t("whatToExpect.waitlistReadMore"),
+      }
+    if (listing.reviewOrderType === ListingReviewOrder.firstComeFirstServe)
+      return { content: t("whatToExpect.fcfs"), expandableContent: t("whatToExpect.fcfsReadMore") }
+
+    return null
+  }
+
+  const whatToExpectContent = getWhatToExpectContent()
 
   const applySidebar = () => (
     <>
@@ -753,7 +774,8 @@ export const ListingView = (props: ListingProps) => {
             )}
             {lotterySection}
             <ExpandableSection
-              content={listing.whatToExpect}
+              content={whatToExpectContent?.content}
+              expandableContent={whatToExpectContent?.expandableContent}
               strings={{
                 title: t("whatToExpect.label"),
                 readMore: t("t.readMore"),
