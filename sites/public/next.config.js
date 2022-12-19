5ad21b1c93186ca6a26bbdf9ff28fa9ec9270038
ColinBuyck <colin.buyck@gmail.com>
2022-12-19 10:36:42 -0800

    fix: gtm naming

--- sites/public/next.config.js
+++ sites/public/next.config.js
@@ -45,7 +45,7 @@ module.exports = withBundleAnalyzer(
       mapBoxToken: MAPBOX_TOKEN,
       housingCounselorServiceUrl: HOUSING_COUNSELOR_SERVICE_URL,
       gaKey: process.env.GA_KEY || null,
-      uaKey: process.env.UA_KEY || null,
+      gtmKey: process.env.GTM_KEY || null,
       idleTimeout: process.env.IDLE_TIMEOUT,
       jurisdictionName: process.env.JURISDICTION_NAME,
       cacheRevalidate: process.env.CACHE_REVALIDATE ? Number(process.env.CACHE_REVALIDATE) : 60,
--- sites/public/src/customScripts.ts
+++ sites/public/src/customScripts.ts
@@ -22,13 +22,13 @@ export const gaCaptureScript = () => {
 }
 
 export const uaScript = () => {
-  const uaKey = process.env.uaKey
-  if (uaKey) {
+  const gtmKey = process.env.gtmKey
+  if (gtmKey) {
     return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':	
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],	
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=	
       '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);	
-      })(window,document,'script','dataLayer','${uaKey}')	
+      })(window,document,'script','dataLayer','${gtmKey}')	
     `
   } else {
     return ""
