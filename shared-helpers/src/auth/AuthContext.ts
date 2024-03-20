7a3f8a3ec29542db905d17f0fcbf2ee874893d4b
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2024-03-20 12:29:28 -0600

    feat: pwdless create account flow (#3964)

--- shared-helpers/src/auth/AuthContext.ts
+++ shared-helpers/src/auth/AuthContext.ts
@@ -385,7 +385,7 @@ export const AuthProvider: FunctionComponent<React.PropsWithChildren> = ({ child
     requestSingleUseCode: async (email) => {
       dispatch(startLoading())
       try {
-        return await authService?.requestSingleUseCode({
+        return await userService?.requestSingleUseCode({
           body: { email },
         })
       } finally {
