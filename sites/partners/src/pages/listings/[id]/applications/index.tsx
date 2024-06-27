b15bf2d4298770b90a7ce69eb16b6d53708b6257
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2024-06-27 10:45:46 -0600

    fix: lottery rebase issues (#4162)

--- sites/partners/src/pages/listings/[id]/applications/index.tsx
+++ sites/partners/src/pages/listings/[id]/applications/index.tsx
@@ -7,6 +7,7 @@ import { AuthContext } from "@bloom-housing/shared-helpers"
 import {
   ApplicationOrderByKeys,
   OrderByEnum,
+  ReviewOrderTypeEnum,
 } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 import {
   useSingleListingData,
@@ -117,6 +118,11 @@ const ApplicationsList = () => {
           flagsQty: flaggedApps?.meta?.totalFlagged,
           listingLabel: t("t.listingSingle"),
           applicationsLabel: t("nav.applications"),
+          lotteryLabel:
+            listingDto?.status === ListingsStatusEnum.closed &&
+            listingDto?.reviewOrderType === ReviewOrderTypeEnum.lottery
+              ? t("listings.lotteryTitle")
+              : undefined,
         }}
         breadcrumbs={
           <Breadcrumbs>
