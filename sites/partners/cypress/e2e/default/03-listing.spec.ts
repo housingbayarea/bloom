9662b390523763f5836fc3953e1004b0f8677ac3
Jared White <jared@jaredwhite.com>
2024-11-26 14:26:03 -0800

    test: remove inapplicable core addition

--- sites/partners/cypress/e2e/default/03-listing.spec.ts
+++ sites/partners/cypress/e2e/default/03-listing.spec.ts
@@ -311,9 +311,6 @@ describe("Listing Management Tests", () => {
     cy.getByID("programRules").contains(listing["programRules"])
     cy.getByID("specialNotes").contains(listing["specialNotes"])
     cy.getByID("reviewOrderQuestion").contains("First come first serve")
-    cy.getByID("whatToExpect").contains(
-      "Applicants will be contacted by the property agent in rank order until vacancies are filled. All of the information that you have provided will be verified and your eligibility confirmed. Your application will be removed from the waitlist if you have made any fraudulent statements. If we cannot verify a housing preference that you have claimed, you will not receive the preference but will not be otherwise penalized. Should your application be chosen, be prepared to fill out a more detailed application and provide required supporting documents."
-    )
     cy.getByID("leasingAgentName").contains(listing["leasingAgentName"])
     cy.getByID("leasingAgentEmail").contains(listing["leasingAgentEmail"].toLowerCase())
     cy.getByID("leasingAgentPhone").contains("(520) 245-8811")
