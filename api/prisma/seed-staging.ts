7f898e9c36979b90cd884774a5eb1c98210bf444
Mateusz Zduniuk <137785676+matzduniuk@users.noreply.github.com>
2025-06-12 10:45:01 +0200

    fix: add preferences conditional rendering

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -81,6 +81,12 @@ export const stagingSeed = async (
   const lakeviewJurisdiction = await prismaClient.jurisdictions.create({
     data: jurisdictionFactory('Lakeview', {
       featureFlags: [
+        FeatureFlagEnum.disableJurisdictionalAdmin,
+        FeatureFlagEnum.disableListingPreferences,
+        FeatureFlagEnum.enableAccessibilityFeatures,
+        FeatureFlagEnum.enableAdditionalResources,
+        FeatureFlagEnum.enableCompanyWebsite,
+        FeatureFlagEnum.enableGeocodingRadiusMethod,
         FeatureFlagEnum.enableUnitGroups,
         FeatureFlagEnum.hideCloseListingButton,
         FeatureFlagEnum.enableHomeType,
