74b394fc07485e1601c251b45f31e797f2cb4ff6
Morgan Ludtke <ludtkemorgan@gmail.com>
2025-07-10 10:10:39 -0500

    fix: hba specific changes

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -88,6 +88,7 @@ export const stagingSeed = async (
         'paperApplication',
         'referralOpportunity',
         'rentalAssistance',
+        'applicationDueDate',
       ],
     }),
   });
