af6a29d93abb361316d1addcd01b72b39e46b34e
krzysztof ziecina <kziecina@airnauts.com>
2025-07-17 15:46:29 +0200

    test: rollback test fix

--- api/src/utilities/application-export-helpers.ts
+++ api/src/utilities/application-export-helpers.ts
@@ -506,6 +506,10 @@ export const getHouseholdCsvHeaders = (
         label: `Household Member (${j}) Last Name`,
       },
       {
+        path: `householdMember.${i}.firstName`,
+        label: `Household Member (${j}) First Name`,
+      },
+      {
         path: `householdMember.${i}.birthDay`,
         label: `Household Member (${j}) Birth Day`,
       },
